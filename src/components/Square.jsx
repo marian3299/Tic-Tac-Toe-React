f41f0959164cd0730d94bd5a91f43ead8e3ee30f
mariana3299 <mariana.ag3299@gmail.com>
2024-07-02 11:44:48 -0600

    feat: uploading project

---
+++ src/components/Square.jsx
@@ -0,0 +1,14 @@
+import '../styles/Square.css'
+
+function Square({ value, onSquareClick, isWinnerSquare, isX, winner }) {
+  return (
+    <button
+      className={`square ${isWinnerSquare ? 'winner':''} ${isX ? 'x':'o'} ${winner ? 'deactivate':''}`}
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default Square;
